dce4caebcbbe4dad2ade95d07ac8cfcdd1020215
“Brenda” <odonnell.brenda@gmail.com>
2019-02-11 17:05:58 +0000

    Created deduct method.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,20 @@ class Oystercard
 
 attr_reader :balance
 
-def initialize
-   @balance = 0
- end
+MAX_LIMIT = 90
 
- def top_up(amount)
-   @balance += amount
- end
+  def initialize
+    @balance = 0
+  end
+
+   def top_up(amount)
+    fail 'Maximum balance exceeded' if amount + balance > MAX_LIMIT
+    @balance += amount
+   end
+
+  def deduct(amount)
+    @balance = balance - amount
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,12 +12,29 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    expect { subject.top_up 2 }.to change{ subject.balance }.by 2
+    # is 1 here a reference to the argument? NO IT IS AN AMOUNT
 
   end
+
+  it 'raises an error if the maximum balance is exceeded' do
+   maximum_balance = Oystercard::MAX_LIMIT
+   # why is there ::?
+   subject.top_up(maximum_balance)
+   expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
+
+ end
+
 end
-  # it 'should top up the balance' do
-  # expect(subject.top_up(10)).to eq true
-  # end
+
+  describe '#deduct' do
+
+  it 'deducts amount from balance' do
+    o = Oystercard.new
+    o.top_up(20)
+    amount = 10
+    expect {subject.deduct(amount)}. to change {subject.balance }.by -amount
+  end
+end
 
 end
