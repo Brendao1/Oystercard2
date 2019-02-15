4c2a96f181367f64bf502fb2f67173b7f55c9c8f
“Brenda” <odonnell.brenda@gmail.com>
2019-02-15 17:09:00 +0000

    Deleted code that was commented out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -25,28 +25,20 @@ FARE = 1
 
   def touch_in(station)
     fail "Insufficient balance to touch in" if @balance < MIN_LIMIT
-    # @in_journey = true  # Journey class
-    # @entry_station = station # Journey class
     @journey = Journey.new(station)
   end
 
   def touch_out(station)
-   # @exit_station = station # Journey class
    if @journey == nil
      then @journey = Journey.new()
    end
     @journey.finish(station)
     @balance -= @journey.fare
     store_trips  # since entry and exit are in Journey, we need a Journey method to return the info about the journey
-   # @entry_station = nil  # Journey class
   end
 
   def store_trips
-    # if @exit_station != nil && @entry_station != nil
       @trips << {entry_station: @journey.entry, exit_station: @journey.exit}
-      # @in_journey = false # Journey class
-    # end
-    # @trips
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,13 +17,11 @@ PENALTY = 6
 
     it 'can top up the balance' do
     expect { subject.top_up 2 }.to change{ subject.balance }.by 2
-    # is 2 here a reference to the argument? NO IT IS AN AMOUNT
 
   end
 
   it 'raises an error if the maximum balance is exceeded' do
    maximum_balance = Oystercard::MAX_LIMIT
-   # why is there ::?
    subject.top_up(maximum_balance)
    expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
 
@@ -41,46 +39,14 @@ end
   end
 end
 
-
-  # describe '#in_journey?' do
-  #   it 'is in journey if the card is in journey' do
-  #     expect(subject.in_journey?).to eq false
-  #     # here we use () instead of {} because it's an argument not a block
-  #   end
-  # end
-
   describe '#touch_in' do
-  # let(:entry_station){ double :station }
-
-  # it 'stores the departure station' do
-  #   o = Oystercard.new
-  #   o.top_up(10)
-  #   o.touch_in(entry_station)
-  #   expect(o.entry_station).to eq entry_station
-  # end
-
-    # it 'touches in and changes the value of in_journey to true' do
-    # o = Oystercard.new
-    # o.top_up(10)
-    # o.touch_in(entry_station)
-    # expect(o.in_journey?).to eq true
-    # # we use the method in_journey? rather than @in_journey because we can't access the variable outside the class
-    # end
 
     it 'will not touch in if below minimum balance' do
       expect{ subject.touch_in(entry_station) }.to raise_error "Insufficient balance to touch in"
     end
-
-
   end
 
   describe '#touch_out' do
-  # let(:exit_station){ double :station }
-
-    # it 'touches out and changes the value of in_journey to false' do
-    # subject.touch_out(exit_station)
-    # expect(subject.in_journey?).to eq false
-    # end
 
     it 'touches out and deducts the fare from the balance' do
     o = Oystercard.new
@@ -94,14 +60,6 @@ end
     o.top_up(90)
     expect { o.touch_out(exit_station) }.to change { o.balance }.by (-PENALTY)
     end
-
-    # it 'touches out and stores the exit station' do
-    #   o = Oystercard.new
-    #   o.top_up(20)
-    #   o.touch_in("Holborn")
-    #   o.touch_out(exit_station)
-    #   expect(o.exit_station).to eq exit_station
-    # end
 
     it 'checks that the default list of trips is empty' do
       o = Oystercard.new
